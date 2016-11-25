b90a1bd10644e972d113e0d3ccd54d9d13ebe8b3
Archie Lee <archielee@appier.com>
2016-11-25 11:34:51 +0800

    Remove js-beautify from executeScript

--- src/background.js
+++ src/background.js
@@ -5,7 +5,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // JS
   chrome.tabs.executeScript(null, { file: 'lib/jquery/jquery.min.js' });
   chrome.tabs.executeScript(null, { file: 'lib/highlight/highlight.min.js' });
-  chrome.tabs.executeScript(null, { file: 'lib/js-beautify/beautify.min.js' });
 
   // Main
   chrome.tabs.executeScript(null, { file: 'src/content_script.js' });
