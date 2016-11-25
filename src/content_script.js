64c1ac8b2ff8c056e855916a142a295504f0d248
Archie Lee <archielee@appier.com>
2016-11-25 17:41:50 +0800

    Use full pack of highlight library

--- src/content_script.js
+++ src/content_script.js
@@ -1,20 +1,31 @@
+'use strict';
+
+var debug = console.log.bind(console, '[jsonz]');
+
 $(document).ready(function(){
+  var body = $('body');
+  var beautifyCode = body.text();
+  var pre = $(document.createElement('pre'));
+  var code = $(document.createElement('code'));
+
+  $('head').append('<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">');
+
   try {
-    const body = $('body');
-    const beautifyCode = JSONStringifyPrettyCompact(JSON.parse(body.text()));
-    const pre = $(document.createElement('pre'));
-    const code = $(document.createElement('code')).text(beautifyCode);
+    beautifyCode = JSONStringifyPrettyCompact(JSON.parse(beautifyCode), { indent: 4 });
+  } catch (e) {
+    debug('failed to parse JSON, fallback to plain text. error:', e);
+  }
 
-    $('head')
-      .append('<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">');
-
+  try {
+    code.text(beautifyCode);
+    
     body
       .empty()
       .append(pre.append(code))
 
     hljs.highlightBlock(body.get(0));
   } catch (e) {
-    console.error('ERROR', e);
+    debug('failed to highlight syntax. error:', e);
   }
 });
 
