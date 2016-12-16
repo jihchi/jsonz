60cd2a548dfa28498911fae40566cd8d947caf79
Archie Lee <archielee@appier.com>
2016-12-16 11:14:35 +0800

    Upgrade wepback to beta version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,6 +5,7 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const ROOT = path.resolve(__dirname);
 const bundledPath = path.join(ROOT, 'bundled');
+const DEV = process.env.NODE_ENV !== 'production';
 
 export default {
   entry: {
@@ -20,16 +21,19 @@ export default {
     path: path.join(ROOT, 'bundled'),
     filename: '[name].bundle.js',
   },
-  resolve: {
-    extensions: ['', '.js', '.css', '.html'],
-    root: ROOT,
-  },
   module: {
-    loaders: [
+    rules: [
       { test: /\.(html|js)$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.html$/, exclude: /node_modules/, loader: 'svelte-loader' },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader' }) },
     ],
+  },
+  devtool: DEV ? 'cheap-module-eval-source-map' : 'source-map',
+  resolve: {
+    extensions: ['.js', '.css', '.html'],
+    alias: {
+      src: path.join(ROOT, 'src'),
+    },
   },
   plugins: [
     new CopyWebpackPlugin([
